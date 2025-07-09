4b86665ca463d0ab1ad98e672f5e6d33515727c8
Nicole Julek Klazura <143097595+nicjulek@users.noreply.github.com>
2025-07-08 22:29:48 -0300

    Teste comunicação com axios

--- src/pages/Pintura.tsx
+++ src/pages/Pintura.tsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { User } from "../types/types";
+import axios from "axios";
 
 // Teste para header
 const testeUser: User = {
@@ -11,7 +12,12 @@ const testeUser: User = {
   role: 'admin'
 };
 
+const testeHello = async () => {
+  await axios.get(`${process.env.REACT_APP_API_URL}/hello-world`)
+}
+
 const Pintura: React.FC = () => {
+  console.log(testeHello());
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header user={testeUser} />
